2b202b7860aada3dc3feae650e3b4e838cf81ffe
Ilya Voloshik <eggestig12353@gmail.com>
2020-08-04 10:32:05 +0300

    !vcx

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,6 +13,25 @@ namespace ConsoleApp1
             Console.WriteLine("You enter this:" );
             Console.ForegroundColor= ConsoleColor.Green;
             Console.WriteLine(txt);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Enter you name:");
+            string name=Console.ReadLine();
+            Console.WriteLine("Youre name:");
+            Console.WriteLine(name);
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Enter youre age:");
+            int age=0;
+            try
+            {
+                 age = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Incorrect Data");
+            }
+            Console.WriteLine("Youre age is:");
+            Console.WriteLine(age);
             Console.ReadLine();
             
         }
