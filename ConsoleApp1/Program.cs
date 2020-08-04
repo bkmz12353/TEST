100759c43427c77cf922f1d13bc1b4aa648aaf36
Ilya Voloshik <eggestig12353@gmail.com>
2020-08-04 10:18:19 +0300

    1 Version

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Enter something:");
+            string txt=Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("You enter this:" );
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine(txt);
+            Console.ReadLine();
+            
+        }
+    }
+}
